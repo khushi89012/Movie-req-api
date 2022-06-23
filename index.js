9a17c3afbe104d2cafef2938dd2c98c6749b7129
khushi Gupta <95909861+khushi89012@users.noreply.github.com>
2022-06-24 01:13:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+
+//dependencies
 const express = require('express')
-const Movie = require("./src/controllers/movie.controller")
-const app = express()
 const cors = require('cors')
+
+// import files
+const Movie = require("./src/controllers/movie.controller")
 const {register,login} =  require("./src/controllers/user.controller")
 const connect = require("./src/config/db")
+
+// create express app
+const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+// routes
 app.use("/",Movie)
 app.post('/register',register)
 app.post("/login",login)
 
 app.listen(8000,async()=>{
-    connect
+    connect // connect to db
     console.log("Server is running on Port 8000")
 
 })
